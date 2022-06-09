f2d6ac51010c5f0ce32868dfa5f238d1fa33641a
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-09 14:38:48 -0500

    solve binary search algo in python and js

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,9 +1,23 @@
-function binarySearch(){
-  // Code goes here!
+function binarySearch(num, arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (num === arr[mid]) {
+      return mid;
+    } else if (num < arr[mid]) {
+      end = mid - 1;
+    } else if (num > arr[mid]) {
+      start = mid + 1;
+    }
+  }
+  return -1;
 }
 
-var smallArray = [1,2,3,4,5]
-var largeArray = [1,5,7,2,3,8,4,9]
+var smallArray = [1, 2, 3, 4, 5];
+var largeArray = [1, 5, 7, 2, 3, 8, 4, 9];
+largeArray.sort();
 
 console.log(binarySearch(1, smallArray) === 0);
 console.log(binarySearch(2, smallArray) === 1);
--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,13 @@
-def binary_search():
-    pass
+def binary_search(num, arr):
+    start = 0
+    end = len(arr)-1
+    
+    while start <= end:
+        mid = (start + end) // 2
+        if num == arr[mid]:
+            return mid
+        elif num < arr[mid]:
+            end = mid -1
+        elif num > arr[mid]:
+            start = mid + 1
+    return -1
